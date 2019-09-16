7f28c557c555bae8d97ce1c5b83be9cec37c2003
Marin <m.marechal@criteo.com>
2019-09-16 18:12:02 +0200

    betageo

---
+++ site/src/components/articles/Article.vue
@@ -0,0 +1,54 @@
+<template lang="html">
+  <article class="">
+    <div class="articleImage">
+      <img :src="__article.img" alt="">
+    </div>
+    <div class="articleContent">
+      <h2> {{ __article.title }} </h2>
+      <p> {{ __article.content }}</p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props:["__article"]
+}
+</script>
+
+<style lang="scss" scoped>
+
+  article {
+    display: flex;
+    height: 100%;
+    // display: none;
+
+    &.active {
+      display: flex;
+    }
+
+    .articleImage {
+      flex: 1;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+
+    .articleContent {
+      flex: 1;
+      margin-left: 20px;
+
+      h2 {
+        text-align: left;
+        margin-bottom: 30px;
+      }
+
+      p {
+        text-align: justify;
+      }
+    }
+  }
+</style>
--- site/src/components/routes/Articles.vue
+++ site/src/components/routes/Articles.vue
@@ -1,50 +1,115 @@
 <template lang="html">
   <div class="articles page">
-    <ul>
-      <li :for="article in articles">
-        <p>{{ article.title }}</p>
+    <div class="articleContainer">
+      <div class="arrow leftArrow" @click="prevArticle"></div>
+      <!-- <transition-group name="slide"> -->
+        <app-article
+          v-for="(article, key, index) in articles"
+          :__article="article"
+          :data-index="index"
+          :v-show="indexArticle === index"
+          :key="index"
+          ></app-article>
+      <!-- </transition-group> -->
 
-
-      </li>
-    </ul>
+      <div class="arrow rightArrow" @click="nextArticle"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import Article from '../articles/Article.vue'
+
 export default {
+  components: {
+    "app-article": Article
+  },
   data() {
     return {
-      articles: [{
-        titre: "coucou",
-        data: "data"
-      }, {
-        titre: "hello",
-        data: "data"
-      }]
+      articles: null,
+      indexArticle: 0
     }
   },
-
-  created() {
-    let that = this;
-    let articlesCollection = {};
-    db.collection('articles').get().then(function(querySnapshot) {
-
-      querySnapshot.forEach(function(doc) {
-        let id = doc.id;
-        let title = doc.data().titre;
-        let img = doc.data().image;
-        let content = doc.data().texte;
-
-        articlesCollection[id] = {};
-        articlesCollection[id]["title"] = title;
-        articlesCollection[id]["img"] = img;
-        articlesCollection[id]["content"] = content;
+  methods: {
+    getArticles() {
+      let that = this;
+      let articlesCollection = {};
+      db.collection('articles').get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          articlesCollection[doc.id] = {};
+          articlesCollection[doc.id]["title"] = doc.data().titre;
+          articlesCollection[doc.id]["img"] = doc.data().image;
+          articlesCollection[doc.id]["content"] = doc.data().texte;
+        });
+      }).then(() => {
+        this.articles = articlesCollection
       });
-    });
-    // this.articles = articlesCollection
+    },
+    nextArticle() {
+      this.indexArticle++;
+    },
+    prevArticle() {
+      this.indexArticle--;
+    }
+  },
+  mounted() {
+    this.getArticles();
   }
 }
 </script>
 
 <style lang="scss" scoped>
+
+.articleContainer {
+    height: 100%;
+    padding: 20px 60px 60px;
+    overflow: hidden;
+}
+
+.arrow {
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    position: fixed;
+    top: 50%;
+    transform: translateY(-50%);
+
+    &::before {
+        content: "";
+        display: block;
+        position: absolute;
+        background-color: black;
+        width: 3px;
+        height: 30px;
+        bottom: 0;
+        left: 50%;
+        transform: rotate(45deg);
+    }
+    &::after {
+        content: "";
+        display: block;
+        position: absolute;
+        background-color: black;
+        width: 3px;
+        height: 30px;
+        top: 0;
+        left: 50%;
+        transform: rotate(-45deg);
+    }
+
+    &.leftArrow {
+        left: 10px;
+        &::before {
+            transform: rotate(-45deg);
+        }
+        &::after {
+            transform: rotate(45deg);
+        }
+    }
+    &.rightArrow {
+        right: 10px;
+    }
+}
+
+
 </style>
